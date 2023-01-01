6fd6d6e61afa6e409e2b5a7cc83a0b8030509c57
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2023-01-02 00:48:19 +0530

    implementing mysql with node js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const sqlConnection = require('./config/db.config.js');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    return res.send('Hello!');
+    sqlConnection.query('SELECT * FROM user_roles', (error, rows, field) => {
+        console.log(rows);
+        return res.send(rows);
+    });
 });
 
 const port = process.env.PORT;
