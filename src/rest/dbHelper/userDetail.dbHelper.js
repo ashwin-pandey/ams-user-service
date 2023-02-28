682e40e4515d09303af8462032733dfb00e4815d
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2023-02-28 23:21:22 +0530

    moved db helper folder inside rest

---
+++ src/rest/dbHelper/userDetail.dbHelper.js
@@ -0,0 +1,17 @@
+const connection = require('../../config/db.config.js');
+const logger = require('../../config/logger.config.js');
+const queryConstant = require('../../constants/query.constant.js');
+
+module.exports = {
+    /** query to get all the user detail records */
+
+    /** query to get a specific user by id */
+
+    /** query to create a new user */
+
+    /** query to update an existing user */
+
+    /** query to de-activate (delete) an existing user */
+
+    /** query to activate the user */
+}
